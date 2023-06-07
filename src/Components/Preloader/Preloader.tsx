635d396c5d8d15fcc01e1f09ad9575e9f16862f0
Ivan <Ivan_567.92@mail.ru>
2023-06-07 12:08:42 +0300

    work layout overview

--- src/Components/Preloader/Preloader.tsx
+++ src/Components/Preloader/Preloader.tsx
@@ -2,9 +2,11 @@ import style from './Preloader.module.scss'
 
 export const Preloader: React.FC = () => {
     return (
-        <div className={style.ldsRipple}>
-            <div></div>
-            <div></div>
+        <div className={style.preloaderWrapper}>
+            <div className={style.ldsRipple}>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     )
 }
