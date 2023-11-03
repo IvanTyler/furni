5d00fb0013b077e5232b2ad14bae350b9a541cef
Ivan <Ivan_567.92@mail.ru>
2023-11-03 15:33:56 +0300

    edit error http

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,17 @@
 import { Navigate } from "react-router-dom";
+import { useTypeSelector } from "../../Hooks/useTypeSelector";
 
 interface IProtectedRouteProps {
     children: any
 }
 
 export function ProtectedRoute({ children }: IProtectedRouteProps) {
+    const { isLoadingAuth } = useTypeSelector(state => state.data)
+
     const getTokenSessionStorage = localStorage.getItem('token')
 
-    if (!getTokenSessionStorage) {
+
+    if (!isLoadingAuth) {
         return <Navigate to="/login" />;
     }
     return children;
